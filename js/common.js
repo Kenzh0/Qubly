58c47199adb8cbd4d89a62a518c17ec4f22d8fe8
Yerassyl Kenzhakhmetov <89932952+Kenzh0@users.noreply.github.com>
2022-12-02 08:48:10 +0000

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -25,4 +25,20 @@ $(function() {
 
 	/* reviews tabs */
 
+	/* yakor animation */
+	$('a.yakor').on('click', function (event) {
+		var $anchor = $(this)
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+		}, {
+			duration: 2000,
+			specialEasing: {
+				width: "linear",
+				height: "easeInOutCubic"
+			}
+		})
+		event.preventDefault()
+	})
+	/* yakor animation */
+
 });
